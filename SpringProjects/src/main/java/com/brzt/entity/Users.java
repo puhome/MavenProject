a071b82acd6475b2f32d5c0973a679d688a7cbf1
puhome <363140622@qq.com>
2020-07-22 17:02:35 +0800

    spring test 4

--- SpringProjects/src/main/java/com/brzt/entity/Users.java
+++ SpringProjects/src/main/java/com/brzt/entity/Users.java
@@ -1,5 +1,7 @@
 package com.brzt.entity;
 
+import java.util.*;
+
 /**
  * 快速封装，右键Generate，Getter and Setter
  */
@@ -8,6 +10,13 @@ public class Users {
     private int id;
     private String name;
     private Car car;
+    private Object obj;
+    private List list;
+    private Set set;
+    private Map map;
+    private Properties prop;
+
+
 
     public int getId() {
         return id;
@@ -31,6 +40,46 @@ public class Users {
 
     public void setCar(Car car) {
         this.car = car;
+    }
+
+    public Object getObj() {
+        return obj;
+    }
+
+    public void setObj(Object obj) {
+        this.obj = obj;
+    }
+
+    public List getList() {
+        return list;
+    }
+
+    public void setList(List list) {
+        this.list = list;
+    }
+
+    public Set getSet() {
+        return set;
+    }
+
+    public void setSet(Set set) {
+        this.set = set;
+    }
+
+    public Map getMap() {
+        return map;
+    }
+
+    public void setMap(Map map) {
+        this.map = map;
+    }
+
+    public Properties getProp() {
+        return prop;
+    }
+
+    public void setProp(Properties prop) {
+        this.prop = prop;
     }
 
     public Users(){}
